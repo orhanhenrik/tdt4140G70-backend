b91377e8af16289b18745105a4ed8bbb7c8b86c6
haakongv@hotmail.com <haakongv@hotmail.com>
2017-02-22 18:23:26 +0100

    add home, search

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.get_courses, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^tdt4140/', views.index, name = 'test'),
+    url(r'^ttm4100/', views.index, name = 'test'),
+    url(r'^tma4165/', views.index, name = 'test'),
+    url(r'^tfe4105/', views.index, name = 'test')
 ]
