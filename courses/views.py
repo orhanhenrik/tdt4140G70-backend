2f613becf105f14a0f5b714b5fe06941e16af5cc
haakongv@hotmail.com <haakongv@hotmail.com>
2017-02-22 19:24:13 +0100

    stuff

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 def get_courses(request):
     return render(request, 'courses/list_courses.html', {
--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_courses, name='index')
+    url(r'^$', views.current_datetime, name='index'),
 ]
