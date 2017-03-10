5eeaab835007448f7283f41e8bb2d7fdeada387f
Joakim Fremstad <joa.fre@gmail.com>
2017-03-10 13:09:05 +0100

    Start of login-system

--- itscrawling/urls.py
+++ itscrawling/urls.py
@@ -17,9 +17,12 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^courses/', include('courses.urls')),
+    url(r'^login/$', auth_views.login, {'template_name': 'general/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^files/', include('files.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
